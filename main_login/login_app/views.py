0c52cc46e3152dc03d27c4832c6db3c2aedf31e8
nikhilrajpk <nikhilrajpk2002@gmail.com>
2024-08-30 17:09:37 +0530

    changed

--- main_login/login_app/views.py
+++ main_login/login_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate
-from django.views.decorators.cache import cache_control
+from django.views.decorators.cache import never_cache
 from django.http import Http404
 # Create your views here.
 
 
-@cache_control(no_cache=True, must_revalidate=True,no_store=True)
+@never_cache
 def user_login(request):
     result = ""
     if 'username' in request.session:
@@ -22,14 +22,14 @@ def user_login(request):
             
     return render(request, 'login.html',{'error':result})
 
-@cache_control(no_cache=True, must_revalidate=True,no_store=True)
+@never_cache
 def home(request):
     if 'username' in request.session:
         return render(request, 'home.html')
     else:
         return redirect(user_login)
 
-@cache_control(no_cache=True, must_revalidate=True,no_store=True)
+@never_cache
 def user_logout(request):
     if request.method != 'POST':
         raise Http404()
